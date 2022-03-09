046718c63ad4b05bc4508083c3f3adcb8c33e293
Haseebk0678 <haseebkhan0678@gmail.com>
2022-03-09 10:42:41 -0500

    fixed unique id error from not removing the table

--- lab_2/script.sql
+++ lab_2/script.sql
@@ -1,6 +1,17 @@
 .headers on
 .mode columns
 
+DROP Table if exists Judges;
+DROP TABLE IF EXISTS Car_Score;
+DROP TABLE IF EXISTS Cars;
+DROP TABLE IF EXISTS Car_Total; 
+DROP TABLE IF EXISTS Car_Ranking;
+DROP TABLE IF EXISTS Rank;
+DROP TABLE IF EXISTS Judges_New;
+
+
+
+
 CREATE TABLE Judges(
     Judge_Time text,
     Judge_ID text,
@@ -35,6 +46,9 @@ CREATE TABLE Car_Total(
    Car_Make text,
    Car_Model text,
    Car_Total int);
+
+INSERT INTO Car_Total SELECT Cars.Car_ID, Cars.Car_Year, Cars.Car_Make, Cars.Car_Model, Car_Score.Car_Score FROM Cars, Car_Score WHERE Cars.Car_ID = Car_Score.Car_ID;
+
 
 CREATE TABLE Car_Ranking(
    Car_ID int,
@@ -89,5 +103,6 @@ CREATE TABLE Judges_New(
 
 
 INSERT INTO Judges_New SELECT Judge_ID, count(*), min(Judge_Time), max(Judge_Time), max(Judge_Time_Min) - min(Judge_Time_Min), (max(Judge_Time_Min) - min(Judge_Time_Min))/count(*) FROM Judges GROUP BY Judge_ID;
+
 
 Drop table Judges;
